c97bdece3769f688b5d8335d1c60840a5fafc944
LeeYarbin <lyb429958@126.com>
2018-03-08 17:55:16 +0800

    1.修复bean组件的一些bug问题 2.整合mybatis的组件问题 3.集成框架并成功测试通过

---
+++ pay_develop/src/main/java/com/pay/schedule/pojo/model/dict/ScheduleExecutionState.java
@@ -0,0 +1,14 @@
+package com.pay.schedule.pojo.model.dict;
+
+/**
+ * @author LiYabin
+ * 执行状态
+ */
+
+public enum ScheduleExecutionState {
+    PREPARE,
+    RUNNING,
+    SUCCESS,
+    FAILURE
+    ;
+}
---
+++ pay_develop/src/main/java/com/pay/schedule/pojo/model/dict/ScheduleWorkType.java
@@ -0,0 +1,17 @@
+package com.pay.schedule.pojo.model.dict;
+
+import com.pay.beans.dictionary.base.BaseDict;
+import com.pay.beans.dictionary.base.extend.DataDict;
+
+public enum ScheduleWorkType{
+
+    /**
+     * 定时模式
+     */
+    CRON_SCHEDULE,
+    /**
+     * 时间间隔模式
+     */
+    SIMPLE_SCHEDULE
+    ;
+}
